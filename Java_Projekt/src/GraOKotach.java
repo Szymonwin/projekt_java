1c646911c61110b18842dca3047dbe9bb0103eb2
m4rianka <marianka.cebula@gmail.com>
2025-03-16 16:02:56 +0100

    Update GraOKotach.java
    dodalam komentarzki

--- Java_Projekt/src/GraOKotach.java
+++ Java_Projekt/src/GraOKotach.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+//glowna klasa - okno z funckja main i actionlistenerem
 public class GraOKotach extends JFrame implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
@@ -15,21 +16,21 @@ public class GraOKotach extends JFrame implements ActionListener {
 	MenuPanel menu;
 	String command;
 	CardLayout cardLayout;
-	int currentCard = 1;
+	int currentCard = 1; //zmienna okreslajaca ktora karta jest otwarta
 	
 	public GraOKotach(String title) {
 		super(title);
-		cardLayout = new CardLayout();
-		cards = new JPanel(cardLayout);
+		cardLayout = new CardLayout(); //cardlayout - panele jedebn pod drugim i mozna zmieniac
+		cards = new JPanel(cardLayout); //panel do przechowywania "kart"
 		this.add(cards);
 		
 		menu = new MenuPanel();
-		cards.add(menu, "1");
+		cards.add(menu, "1"); //panel menu dodaje do kart z indeksem 1
 		
 		mainP = new MainPanel();
 		cards.add(mainP, "2");
 		
-		menu.start.addActionListener(this);
+		menu.start.addActionListener(this); //dodaje actionlisterery do guzikow w dwoch pozostalych klasach
 		menu.exit.addActionListener(this);
 		mainP.back.addActionListener(this);
 	}
@@ -38,17 +39,17 @@ public class GraOKotach extends JFrame implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		command = e.getActionCommand();
 		
-		switch(command) {
+		switch(command) { //switch jest wuygodniejsze od for w tym przypadku, bo bedzie duzo guzikow
 		case "start":
 			currentCard = 2;
-			cardLayout.show(cards, "" + currentCard);
+			cardLayout.show(cards, "" + currentCard); //wyswietlamy karte z indeksem 1
 			break;
 		case "exit":
 			System.exit(0);
 			break;
 		case "back":
 			currentCard = 1;
-			cardLayout.show(cards, "" + currentCard);
+			cardLayout.show(cards, "" + currentCard); //wyswietlamy karte z indeksem 2
 			break;
 		}
 

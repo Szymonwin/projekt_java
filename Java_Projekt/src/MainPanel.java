d75d78e0bf9a2e776a6233beff416d0f2d2e2e54
m4rianka <marianka.cebula@gmail.com>
2025-03-16 15:54:08 +0100

    Update MainPanel.java
    dodalam komentarze

--- Java_Projekt/src/MainPanel.java
+++ Java_Projekt/src/MainPanel.java
@@ -9,6 +9,7 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+//ekran glowny gry
 public class MainPanel extends JPanel {
 	private static final long serialVersionUID = 1L;
 	
@@ -17,20 +18,20 @@ public class MainPanel extends JPanel {
 	
 	MainPanel(){
 		super();
-		
+
+		//panel w panelu zeby miec taki gorny do guziczkow
 		panelGorny = new JPanel(new FlowLayout(FlowLayout.LEFT));
 		panelGorny.setBackground(Color.pink);
-		
+
 		this.setLayout(new BorderLayout());
-		this.setBorder(new EmptyBorder(new Insets(15, 15, 15, 15)));
+		this.setBorder(new EmptyBorder(new Insets(15, 15, 15, 15))); //to sa takie jakby marginesy
 		
 		this.add(panelGorny, BorderLayout.BEFORE_FIRST_LINE);
 		
 		back = new JButton("BACK");
 		back.setActionCommand("back");
-		back.setBounds(15,15,60,15);
+		back.setBounds(15,15,60,15); //rozmiar i miejsce guzika (x,y,szer,wys)
 		panelGorny.add(back);
-		
 		
 		this.setBackground(Color.PINK);
 	}

65954970bc3a56ab1c291c0846ff3fddfd3ff40c
m4rianka <marianka.cebula@gmail.com>
2025-03-16 15:58:32 +0100

    Update MenuPanel.java
    dodalam komentarze

--- Java_Projekt/src/MenuPanel.java
+++ Java_Projekt/src/MenuPanel.java
@@ -13,20 +13,20 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+//menu gry
 public class MenuPanel extends JPanel{
-	
 	private static final long serialVersionUID = 1L;
-	
-	public JButton start, exit, next;
+
+	public JButton start, exit, next; //guziki musza byc publiczne, zeby dzialal actionlistener w innej klasie
 	BoxLayout boxLayout;
 	JPanel panelSrodkowy, panelZGuzikami; 
 	JLabel napis;
 	
 	public MenuPanel() {
 		super();
-		panelSrodkowy = new JPanel(new BorderLayout());
+		panelSrodkowy = new JPanel(new BorderLayout());//panel ktory jest na srodku, ma napis i panel z guzikami
 		panelSrodkowy.setBackground(Color.PINK);
-		panelZGuzikami = new JPanel(new GridLayout(2,1,0,10));
+		panelZGuzikami = new JPanel(new GridLayout(2,1,0,10));//gridlayout(rzedy,kolumny,przerwy poziome,przerwy pionowe)
 		panelZGuzikami.setBackground(Color.PINK);
 		
 		start = new JButton("START");
@@ -38,16 +38,16 @@ public class MenuPanel extends JPanel{
 		panelZGuzikami.add(exit);
 		
 		napis = new JLabel("Gra o kotach");
-		napis.setFont(new Font("Serif", Font.BOLD, 20));
+		napis.setFont(new Font("Serif", Font.BOLD, 20));//zmiana czcionki i tak dalej - ta jest MEGA brzydka musi8my potem zmienic
 		
 		panelSrodkowy.add(napis, BorderLayout.PAGE_START);
 		panelSrodkowy.add(panelZGuzikami, BorderLayout.CENTER);
-		
-		boxLayout = new BoxLayout(this, BoxLayout.Y_AXIS);
-		this.setLayout(boxLayout);
-		this.setBorder(new EmptyBorder(new Insets(100, 100, 100, 100)));
+
+		//boxlayout - komponenty pod soba lub obok, ale jednak poradziloam sobie inaczej 
+		//boxLayout = new BoxLayout(this, BoxLayout.Y_AXIS);
+		//this.setLayout(boxLayout);
+		this.setBorder(new EmptyBorder(new Insets(100, 100, 100, 100))); //marginesy
 		this.add(panelSrodkowy);
-		
 		
 		this.setBackground(Color.PINK);
 	}

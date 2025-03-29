a09e0b56e38759d3fcf072320eafdb016e6b42dc
Szymonwin <szymdzio@gmail.com>
2025-03-29 19:41:43 +0100

    klasa food

--- Java_Projekt/src/pl/edu/pw/fizyka/pojava/WinnickiCebula/Food.java
+++ Java_Projekt/src/pl/edu/pw/fizyka/pojava/WinnickiCebula/Food.java
@@ -1,13 +1,13 @@
 //Autor: Szymon
 package pl.edu.pw.fizyka.pojava.WinnickiCebula;
-
+//klasa przechowywująca informacje o jedzeniu
 public class Food {
 	
 	protected String name;
 	protected int foodPoints;
 	
-	public Food(){
-		
+	public Food(String name, int foodPoints){
+		this.name = name;
+		this.foodPoints = foodPoints;
 	}
-
 }
